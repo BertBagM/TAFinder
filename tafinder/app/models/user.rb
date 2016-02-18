5553827fd2dba7b16547a06dc2741e3ad8492bc7
Scott Mielcarski <scott_mielcarski@me.com>
2016-02-17 16:15:45 -0800

    Add authentication to user model

--- tafinder/app/models/user.rb
+++ tafinder/app/models/user.rb
@@ -4,9 +4,38 @@ class User < ActiveRecord::Base
     uniqueness: true,
     length: { in: 3..50 },
     format: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
-  validates :password
+  validates :password,
     presence: true,
     confirmation: true,
     length: { in: 6..50 },
     on: :create
+
+  before_save :encrypt_password
+  after_save :clear_password
+
+
+  def self.authenticate(email = "", login_password = "")
+    user = User.find_by_email(email)
+
+    if (user && user.match_password(login_password))
+      return user
+    else
+      return false
+    end
+  end
+
+  def encrypt_password
+    if (self.password_changed?())
+      self.password_salt = BCrypt::Engine.generate_salt()
+      self.password = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def clear_password
+    self.password = nil
+  end
+
+  def match_password(login_password = "")
+    password == BCrypt::Engine.hash_secret(login_password, password_salt)
+  end
 end
