7e6356ddf8a5efb65a2d1a771e1ee496a6421abe
Scott Mielcarski <scott_mielcarski@me.com>
2016-02-18 09:59:04 -0800

    Display an alert when attempting to visit a page without the correct permissions

--- tafinder/app/controllers/admin_controller.rb
+++ tafinder/app/controllers/admin_controller.rb
@@ -1,8 +1,3 @@
 class AdminController < ApplicationController
   before_action :validate_logged_in
-
-
-  def validate_logged_in
-    redirect_to('/') unless logged_in?
-  end
 end
--- tafinder/app/controllers/application_controller.rb
+++ tafinder/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def validate_logged_in
+    unless logged_in?
+      flash[:danger] = 'You do not have the correct permissions to view that page'
+      redirect_to('/')
+    end
+  end
 end
