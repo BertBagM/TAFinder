29e074a25a50757bcfe285d3707faae155074661
Paul Myhr <Goreng@Pauls-MacBook-Pro.local>
2016-02-19 00:19:00 -0800

    add attributes to application migrate create

--- tafinder/db/migrate/20160213012712_create_applications.rb
+++ tafinder/db/migrate/20160213012712_create_applications.rb
@@ -1,6 +1,26 @@
 class CreateApplications < ActiveRecord::Migration
   def change
     create_table :applications do |t|
+    	t.integer	:studentNum
+    	t.string	:firstName
+    	t.string	:lastName
+    	t.string	:emailAddress
+    	t.decimal :GPA
+    	t.string	:faculty
+    	t.integer	:yearOfStudy
+    	t.boolean	:graduateStudent
+    	t.integer	:ubcEmployeeId
+    	t.string	:program
+    	t.string	:gender
+    	t.string	:streetAddress
+    	t.string	:city
+    	t.string	:postalCode
+    	t.string	:homePhone
+    	t.string	:cellPhone
+    	t.boolean	:graduateFTStatus
+    	t.boolean	:previousUTAPosition
+    	t.integer	:preferredHours
+    	t.integer	:maximumHours
 
       t.timestamps null: false
     end
