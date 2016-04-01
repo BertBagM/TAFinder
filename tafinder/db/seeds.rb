0af1dd0f9353b649de6576f815d39e40e5e0367b
Scott Mielcarski <scott_mielcarski@me.com>
2016-03-31 18:09:20 -0700

    Add development only seed data for terms, courses, and sections

--- tafinder/db/seeds.rb
+++ tafinder/db/seeds.rb
@@ -6,9 +6,40 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+# TODO(scott): add proper admin account before deploying
 users = User.create([
   {
     email: "admin@admin.com",
     password: "password"
   }
 ])
+
+if (Rails.env == "development")
+  terms = Term.create([
+    {
+      year: "2016",
+      semester: "W",
+      open: true
+    }
+  ])
+
+  courses = terms.first.courses.create([
+    {
+      subject: "COSC",
+      number: "310"
+    }
+  ])
+
+  sections = courses.first.sections.create([
+    {
+      number: "L01",
+      instructor_name: "James",
+      act_type: "Lab",
+      days: "WT",
+      lab_hours: 4,
+      enrolled: 100,
+      building: "SCI"
+    }
+  ])
+end
