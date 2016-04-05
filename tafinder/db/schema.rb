02e2a70d2d8191a286292cbaa3f5214d60152d88
Paul Myhr <Goreng@host194-116.vpn.ubc.ca>
2016-04-05 15:01:22 -0700

    change graduate_full_time to full_time

--- tafinder/db/schema.rb
+++ tafinder/db/schema.rb
@@ -14,29 +14,29 @@
 ActiveRecord::Schema.define(version: 20160330003216) do
 
   create_table "applications", force: :cascade do |t|
-    t.integer  "student_id",                                                            null: false
-    t.string   "first_name",                                                            null: false
-    t.string   "last_name",                                                             null: false
-    t.string   "email",                                                                 null: false
-    t.decimal  "gpa",                           precision: 5, scale: 2
-    t.string   "faculty",                                                               null: false
-    t.integer  "study_year",                                            default: 1,     null: false
-    t.boolean  "graduate",                                              default: false, null: false
-    t.boolean  "graduate_full_time",                                    default: false, null: false
+    t.integer  "student_id",                                                         null: false
+    t.string   "first_name",                                                         null: false
+    t.string   "last_name",                                                          null: false
+    t.string   "email",                                                              null: false
+    t.decimal  "gpa",                        precision: 5, scale: 2
+    t.string   "faculty",                                                            null: false
+    t.integer  "study_year",                                         default: 1,     null: false
+    t.boolean  "graduate",                                           default: false, null: false
+    t.boolean  "full_time",                                          default: false, null: false
     t.integer  "ubc_employee_id"
     t.string   "program"
-    t.string   "gender",                                                                null: false
-    t.string   "street",                                                                null: false
-    t.string   "city",                                                                  null: false
-    t.string   "postal_code",                                                           null: false
-    t.string   "home_phone",         limit: 14
-    t.string   "cell_phone",         limit: 14
-    t.boolean  "previous_ta",                                           default: false, null: false
-    t.integer  "preferred_hours",                                       default: 12,    null: false
-    t.integer  "maximum_hours",                                         default: 12,    null: false
-    t.integer  "term_id",                                                               null: false
-    t.datetime "created_at",                                                            null: false
-    t.datetime "updated_at",                                                            null: false
+    t.string   "gender",                                                             null: false
+    t.string   "street",                                                             null: false
+    t.string   "city",                                                               null: false
+    t.string   "postal_code",                                                        null: false
+    t.string   "home_phone",      limit: 14
+    t.string   "cell_phone",      limit: 14
+    t.boolean  "previous_ta",                                        default: false, null: false
+    t.integer  "preferred_hours",                                    default: 12,    null: false
+    t.integer  "maximum_hours",                                      default: 12,    null: false
+    t.integer  "term_id",                                                            null: false
+    t.datetime "created_at",                                                         null: false
+    t.datetime "updated_at",                                                         null: false
   end
 
   create_table "courses", force: :cascade do |t|
